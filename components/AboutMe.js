02745da9bb0b1257cc702958d053dab62763c523
Adam <adampeleback@gmail.com>
2024-04-16 21:22:55 +0200

    removed java from tech stack

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -119,10 +119,6 @@ export default function AboutMe() {
                 className="h-20 w-20 mx-4 my-4"
               />
               <img
-                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png"
-                className="h-20 w-20 mx-4 my-4"
-              />
-              <img
                 src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png"
                 className="h-20 w-20 mx-4 my-4"
               />
