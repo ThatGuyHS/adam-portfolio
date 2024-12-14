3089ff3a35d6293625d296c3b8f2030fa698661f
Adam <adampeleback@gmail.com>
2024-12-14 23:26:11 +0100

    redeploy

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -4,7 +4,7 @@ const ProjectPage = ({ project }) => {
   return (
     <div className="container mx-auto px-4">
       <header className="text-center my-6">
-        <h1 className="text-4xl font-bold text-gray-800">{project.title}</h1>
+        <h1 className="text-4xl font-bold text-gray-800">{project?.title}</h1>
       </header>
       <div className="flex justify-center my-6">
         <img src={project.imageUrl} alt={project?.title} className="max-w-md rounded-lg shadow-md" />
