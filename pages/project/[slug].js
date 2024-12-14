ac2c33d5541a709e267a24af71f8238ca2fc725f
Adam <adampeleback@gmail.com>
2024-12-14 23:24:37 +0100

    trying to fix build

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -7,7 +7,7 @@ const ProjectPage = ({ project }) => {
         <h1 className="text-4xl font-bold text-gray-800">{project.title}</h1>
       </header>
       <div className="flex justify-center my-6">
-        <img src={project.imageUrl} alt={project.title} className="max-w-md rounded-lg shadow-md" />
+        <img src={project.imageUrl} alt={project?.title} className="max-w-md rounded-lg shadow-md" />
       </div>
       <article className="prose lg:prose-xl mx-auto text-gray-700">
         <p>{project.description}</p>
